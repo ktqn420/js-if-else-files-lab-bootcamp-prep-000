53bff5e607ad6d31fd5fb5e3ba5c02b4a3267aae
Anna Zhang <quinnkt9@gmail.com>
2017-08-29 23:01:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
-if (name === "Joe") {
+if (name === "susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
